31141a19e29d1c16d63ca431831f04a91feddcb3
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-07-29 13:13:03 -0400

    Removes extra properties and methods, leaving only state property and methods to manipulate state

--- game-of-life/src/components/CellObject.js
+++ game-of-life/src/components/CellObject.js
@@ -1,18 +1,11 @@
 export default class CellObject {
-  constructor(state = false, disabled = false) {
+  constructor(state = false) {
     this.state = state;
-    this.disabled = disabled;
   }
   getState() {
     return this.state;
   }
-  getDisabled() {
-    return this.disabled;
-  }
   setState(value) {
     this.state = value;
-  }
-  setDisabled(value) {
-    this.disabled = value;
   }
 }
